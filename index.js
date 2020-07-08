d3a5fdf84775fbbb9dd7bd74ceeefe409e141b38
shivamVR46 <shivamcharusat@gmail.com>
2020-07-08 13:05:35 -0400

    updated express and body-parser

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ restService.post("/getSongByName", function(req, res) {
 //  var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.song ?
 //                "Shivam" : "Sadiq";
 
-  const speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>a cat purring</desc></audio></speak>';
+  // const speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>a cat purring</desc></audio></speak>';
 
-  // var speech ="";
+  var speech ="";
 
-  // switch(req.body.queryResult.parameters.song){
-  //   case "music":
-  //    speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>a cat purring</desc></audio></speak>';
-  //   case "other":     
-  //    speech = '<speak><audio><desc>a cat purring</desc></audio></speak>';
-  // }
+  switch(req.body.queryResult.parameters.song){
+    case "music":
+     speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>a cat purring</desc></audio></speak>';
+    case "other":     
+     speech = '<speak><audio><desc>a cat purring</desc></audio></speak>';
+  }
 
 
   // '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg"><desc>a cat purring</desc></audio></speak>';

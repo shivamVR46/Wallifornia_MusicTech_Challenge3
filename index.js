bbd5da5d1e6cd1634272dd3f69bda4a35c4e1d7b
shivamVR46 <shivamcharusat@gmail.com>
2020-07-08 02:35:31 -0400

    updated express and body-parser

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ restService.use(bodyParser.json());
 
 restService.post("/getSongByName", function(req, res) {
 
- var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.song ?
-               "Shivam" : "Sadiq";
+//  var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.song ?
+              //  "Shivam" : "Sadiq";
 
-  // const speech = 
-  // '<speak><audio src="https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"><desc>a cat purring</desc></audio></speak>';
+  const speech = 
+  '<speak><audio src=https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg""><desc>a cat purring</desc></audio></speak>';
 
-  //                new Audio('file:///D://file_example_MP3_700KB.mp3') : "No such song";
+                 new Audio('file:///D://file_example_MP3_700KB.mp3') ;
 
   // const song = req.body.result.parameters.song.toLowerCase();
   // const speech1 = new Audio('https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3');

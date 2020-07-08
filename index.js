6cf3a0c31c1fca3b18ef8a6403f22940192c764d
shivamVR46 <shivamcharusat@gmail.com>
2020-07-08 15:32:26 -0400

    updated express and body-parser

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ restService.use(
 
 restService.use(bodyParser.json());
 
+var history = [];
+
 restService.post("/getSongByName", function(req, res) {
 
 //  var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.song ?
@@ -22,14 +24,27 @@ restService.post("/getSongByName", function(req, res) {
   // const speech = "test 1.0"
   var speech ="";
 
-  switch(req.body.queryResult.parameters.song){
+
+  switch(req.body.queryResult.parameters.song){ 
+    
+  
+    
     case "music":
      speech = '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg"><desc>a cat purring</desc></audio></speak>';
+     history.push("cartoon");
      break;
-    case "other":     
+    case "too long":
+      speech = '<speak><audio src="https://cdns-preview-d.dzcdn.net/stream/c-ddf495316e2afbe4327d9a6e17840a69-7.mp3"><desc> Too Long</desc></audio></speech>';
+      history.push("too_long");
+      break;
+    case "dz":     
      speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>deezer stream</desc></audio></speak>';
+     history.push("dz");
      break;
+
+
   }
+
 
   
 
@@ -68,7 +83,50 @@ restService.post("/getSongByName", function(req, res) {
   });
 });
 
+restService.post("/discoverMusic", function(req, res){
+  var speech ="";
+  var musicToDiscover = "";
+  var str = "Looking at your playing history you can discover ";
+  switch(req.body.queryResult.parameters.discover){ 
+    case "discover new song":
+      if(history.indexOf("cartoon")==-1){
+        musicToDiscover = "cartoon";
+      }
+      else if(history.indexOf("too_long")==-1){
+        musicToDiscover = "too long";
+      }
+      else if(history.indexOf("dz")==-1){
+        musicToDiscover = "dz";
+      }
+      speech =  str.concat(musicToDiscover);
+  }
 
+
+  var speechResponse = {  
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: speech
+            }
+          }
+        ]
+      }
+    }
+  };
+
+  return res.json({
+    payload: speechResponse,
+    //data: speechResponse,
+   fulfillmentText: speech,
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+  
+});
 
 restService.listen(process.env.PORT || 8000, function() {
     console.log("Server up and listening");

7c8051fa511dad8f9f7c73e520ac02bcc9647771
shivamVR46 <shivamcharusat@gmail.com>
2020-07-08 17:43:05 -0400

    updated express and body-parser

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ restService.post("/getSongByName", function(req, res) {
     case "music":
      speech = '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg"><desc>a cat purring</desc></audio></speak>';
      break;
-    case "too ":
+    case "too":
       speech = '<speak><audio src= "'+trackThree+'"><desc>Too long</desc></audio></speech>'; 
       break;
-    case "describe too ":
+    case "describe too":
       speech =  '<speak>Track name : '+trackNameThree+' by Artist name : '+artistThree+' from Album : '+albumThree+' </speak>';
     case "other":     
      speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>deezer stream</desc></audio></speak>';

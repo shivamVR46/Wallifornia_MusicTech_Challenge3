7ff073f91b9ad44a1b6ca3a83de55e83db55e949
shivamVR46 <shivamcharusat@gmail.com>
2020-07-08 12:56:51 -0400

    updated express and body-parser

--- index.js
+++ index.js
@@ -17,15 +17,19 @@ restService.post("/getSongByName", function(req, res) {
 
 //  var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.song ?
 //                "Shivam" : "Sadiq";
-  
-  var speech ="";
 
-  switch(req.body.queryResult.parameters.song){
-    case "music":
-     speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>a cat purring</desc></audio></speak>';
-    case "other":     
-     speech = '<speak><audio><desc>a cat purring</desc></audio></speak>';
-  }
+  var speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>a cat purring</desc></audio></speak>';
+
+  // var speech ="";
+
+  // switch(req.body.queryResult.parameters.song){
+  //   case "music":
+  //    speech = '<speak><audio src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3"><desc>a cat purring</desc></audio></speak>';
+  //   case "other":     
+  //    speech = '<speak><audio><desc>a cat purring</desc></audio></speak>';
+  // }
+
+  
   // '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg"><desc>a cat purring</desc></audio></speak>';
                //  new Audio('file:///D://file_example_MP3_700KB.mp3') ;
 

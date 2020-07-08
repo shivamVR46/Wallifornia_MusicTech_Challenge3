2ee3d5b066c1b49b30e0955e2afac032e3444a8d
shivamVR46 <shivamcharusat@gmail.com>
2020-07-08 01:03:08 -0400

    updated express and body-parser

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ restService.use(bodyParser.json());
 
 restService.post("/getSongByName", function(req, res) {
 
-  // var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.song ?
+  var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.song ?
+                "Shivam" : "Sadiq";
   //                new Audio('file:///D://file_example_MP3_700KB.mp3') : "No such song";
 
-  const song = req.body.result.parameters.song.toLowerCase();
-  const speech1 = new Audio('https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3');
-  var speech = "";
+  // const song = req.body.result.parameters.song.toLowerCase();
+  // const speech1 = new Audio('https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3');
+  // var speech = "";
 
-  speech = 'shivam';
+  // speech = 'shivam';
 
   // var speechResponse = {
   //   google: {
